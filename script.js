15108626531f7b1fb3969fa779b75bea30d63f89
Noah Msekni <msekninoah@gmail.com>
2022-05-10 19:29:23 +0200

    Finished the exercise, will clean it up tomorrow

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
-let gridNumber = 16;
+let number = 16;
+let gridNumber = number * number
 
 function promptFunction(){
-    gridNumber = prompt('how many?')
+    number = prompt('how many?')
+    let gridNumber = number * number
         gridFunc(gridNumber)
+        gridMaker()
 }
 
+function gridMaker() {
+    document.getElementById('square-container').style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+}
+gridMaker()
 
 const newGrid = document.querySelector('#newGrid')
 newGrid.onclick = () => {
@@ -25,7 +32,6 @@ function gridFunc(gridNumber){
     for(x=1; x<=gridNumber;x++) {
         const squarediv = document.createElement('div');
         squarediv.className = "square-" + x;
-        squarediv.textContent = 'yo this is square ' + x
 
         squarediv.addEventListener('mouseover', function(){
             squarediv.style = 'background: blueviolet'
